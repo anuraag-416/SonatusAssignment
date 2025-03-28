eabecbc59011e52411687836d23f7d213ab72c8d
anuraagramineni <anuraagfederer071@gmail.com>
2025-03-27 21:11:23 -0700

    initial commit

---
+++ src/main/java/com/example/sonatus/SonatusApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonatusApplication.class, args);
+    }
+
+}
